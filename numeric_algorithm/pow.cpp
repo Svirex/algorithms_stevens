7965d93d0d71d40edc208bebf80148f1fe14d552
Artyom Vorobyev <vorobyev.ae.ser@sberbank.ru>
2019-10-29 09:27:20 +0400

    Проверка на выход из диапазона при умножении

--- numeric_algorithm/pow.cpp
+++ numeric_algorithm/pow.cpp
@@ -4,22 +4,24 @@
 #include <climits>
 #include <cmath>
 #include <iostream>
-#include <vector>
 #include <stdexcept>
+#include <vector>
 
 using ull = unsigned long long;
+
+bool ExceedLimit(ull a, ull b, ull limit) { return a > limit / b; }
 
 void FillingPowsVector(ull a, unsigned pow, std::vector<ull> &pows) {
   pows.emplace_back(a);
   unsigned startPow = 2;
   while (startPow < pow) {
-    // TODO insert ExceedLimit
+    if (ExceedLimit(pows.back(), pows.back(), ULLONG_MAX)) {
+      throw std::length_error("Exceed limit");
+    }
     pows.emplace_back(static_cast<ull>(std::pow(a, startPow)));
     startPow *= 2;
   }
 }
-
-bool ExceedLimit(ull a, ull b, ull limit) { return a > limit / b; }
 
 ull CalcNumber(ull num, ull pow) {
   std::vector<ull> pows;
@@ -27,17 +29,13 @@ ull CalcNumber(ull num, ull pow) {
   ull answer = 1;
   for (size_t i = 0; i < pows.size(); ++i) {
     if (((pow >> i) & 1) == 1) {
-      if (!ExceedLimit(answer, pows[i], ULLONG_MAX)) {
-        answer *= pows[i];
-      } else {
-        std::cerr << "Exceed limit " << ULLONG_MAX;
-        break;
+      if (ExceedLimit(answer, pows[i], ULLONG_MAX)) {
+        throw std::length_error("Exceed limit");
       }
+      answer *= pows[i];
     }
   }
   return answer;
 }
 
-int main() {
-  std::cout << CalcNumber(15, 300) << std::endl;
-}
+int main() { std::cout << CalcNumber(15, 300) << std::endl; }
